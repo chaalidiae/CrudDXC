845fba2ff5d8016c2a5ca3bc3d720d8f9bb00407
chaali <d.chaali1998@gmail.com>
2020-02-14 17:02:16 +0100

    wa 3ina

--- src/services/contacts.service.ts
+++ src/services/contacts.service.ts
@@ -4,27 +4,27 @@ import { map } from 'rxjs/operators';
 import {Contact} from '../model/model.contact';
 @Injectable()
 export class ContactsService {
-  port:string="8084";
+  port = '8080';
   constructor(public http: Http) {
   }
   getContacts(motCle: string, page: number, size: number ) {
-    return this.http.get('http://localhost:'+this.port+'/chercherContact?mc=' + motCle + '&size=' + size + '&page=' + page)
+    return this.http.get('http://localhost:' + this.port + '/chercherContact?mc=' + motCle + '&size=' + size + '&page=' + page)
       .pipe(map(resp => resp.json()));
   }
   saveContact(contact: Contact) {
-    return this.http.post('http://localhost:'+this.port+'/contacts', contact)
+    return this.http.post('http://localhost:' + this.port + '/contacts', contact)
       .pipe(map(resp => resp.json()));
   }
   getContact(id: number) {
-    return this.http.get('http://localhost:'+this.port+'/contacts/' + id)
+    return this.http.get('http://localhost:' + this.port + '/contacts/' + id)
       .pipe(map(resp => resp.json()));
   }
   updateContact(contact: Contact) {
-    return this.http.put('http://localhost:'+this.port+'/contacts/' + contact.id, contact)
+    return this.http.put('http://localhost:' + this.port + '/contacts/' + contact.id, contact)
       .pipe(map(resp => resp.json()));
   }
   deleteContact(id: number) {
-    return this.http.delete('http://localhost:'+this.port+'/contacts/' + id)
+    return this.http.delete('http://localhost:' + this.port + '/contacts/' + id)
       .pipe(map(resp => resp.json()));
   }
 }
