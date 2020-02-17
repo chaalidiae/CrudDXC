8ff5f824488b2b2224e49b154e3fb2f99e986691
chaali <d.chaali1998@gmail.com>
2020-02-17 10:05:33 +0100

    lundi matin push

--- src/services/contacts.service.ts
+++ src/services/contacts.service.ts
@@ -8,7 +8,7 @@ export class ContactsService {
   constructor(public http: Http) {
   }
   getContacts(motCle: string, page: number, size: number ) {
-<<<<<<< HEAD
+
     return this.http.get('http://localhost:' + this.port + '/chercherContact?mc=' + motCle + '&size=' + size + '&page=' + page)
       .pipe(map(resp => resp.json()));
   }
@@ -25,26 +25,6 @@ export class ContactsService {
       .pipe(map(resp => resp.json()));
   }
   deleteContact(id: number) {
-    return this.http.delete('http://localhost:' + this.port + '/contacts/' + id)
-=======
-    return this.http.get('http://localhost:8082/chercherContact?mc=' + motCle + '&size=' + size + '&page=' + page)
-      .pipe(map(resp => resp.json()));
-  }
-  saveContact(contact: Contact) {
-    return this.http.post('http://localhost:8082/contacts', contact)
-      .pipe(map(resp => resp.json()));
-  }
-  getContact(id: number) {
-    return this.http.get('http://localhost:8082/contacts/' + id)
-      .pipe(map(resp => resp.json()));
-  }
-  updateContact(contact: Contact) {
-    return this.http.put('http://localhost:8082/contacts/' + contact.id, contact)
-      .pipe(map(resp => resp.json()));
-  }
-  deleteContact(id: number) {
-    return this.http.delete('http://localhost:8082/contacts/' + id)
->>>>>>> 2058783b6455b6547c7b2665fdf1de9baaeb8195
-      .pipe(map(resp => resp.json()));
+    return this.http.delete('http://localhost:' + this.port + '/contacts/' + id);
   }
 }
